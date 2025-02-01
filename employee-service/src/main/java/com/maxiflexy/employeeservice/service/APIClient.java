2429c8051daae4f076a7c420bc9d8e0617247651
Maxiflexy <onyemax247@gmail.com>
2025-02-01 15:14:24 +0100

    added wenClient and feignClient to make api call

---
+++ employee-service/src/main/java/com/maxiflexy/employeeservice/service/APIClient.java
@@ -0,0 +1,14 @@
+package com.maxiflexy.employeeservice.service;
+
+import com.maxiflexy.employeeservice.dto.DepartmentDTO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
+@FeignClient(url = "localhost:8080", value = "DEPARTMENT-SERVICE")
+public interface APIClient {
+
+    @GetMapping("/api/departments/{department-code}")
+    DepartmentDTO getDepartment(@PathVariable("department-code") String code);
+}
--- employee-service/src/main/java/com/maxiflexy/employeeservice/service/impl/EmployeeServiceImpl.java
+++ employee-service/src/main/java/com/maxiflexy/employeeservice/service/impl/EmployeeServiceImpl.java
@@ -5,11 +5,13 @@ import com.maxiflexy.employeeservice.dto.DepartmentDTO;
 import com.maxiflexy.employeeservice.dto.EmployeeDTO;
 import com.maxiflexy.employeeservice.entity.Employee;
 import com.maxiflexy.employeeservice.repository.EmployeeRepository;
+import com.maxiflexy.employeeservice.service.APIClient;
 import com.maxiflexy.employeeservice.service.EmployeeService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
 @AllArgsConstructor
@@ -17,6 +19,8 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     private EmployeeRepository employeeRepository;
     private RestTemplate restTemplate;
+    private WebClient webClient;
+    private APIClient feignClient;
 
     @Override
     public EmployeeDTO saveEmployee(EmployeeDTO employeeDTO) {
@@ -51,10 +55,24 @@ public class EmployeeServiceImpl implements EmployeeService {
                 employee.getEmail(),
                 employee.getDepartmentCode());
 
+        // using rest template to make a get request call
         ResponseEntity<DepartmentDTO> responseEntity = restTemplate.getForEntity
                 ("http://localhost:8080/api/departments/" + employee.getDepartmentCode(), DepartmentDTO.class);
+        //DepartmentDTO departmentDTO = responseEntity.getBody();
 
-        DepartmentDTO departmentDTO = responseEntity.getBody();
+
+
+        // using a web client from webflux to make a get request call
+//        DepartmentDTO departmentDTO = webClient.get()
+//                .uri("http://localhost:8080/api/departments/" + employee.getDepartmentCode())
+//                .retrieve()
+//                .bodyToMono(DepartmentDTO.class)
+//                .block();
+
+
+
+        // using feign client to make a get request call
+        DepartmentDTO departmentDTO = feignClient.getDepartment(employee.getDepartmentCode());
 
         ApiResponseDTO apiResponseDTO = new ApiResponseDTO();
         apiResponseDTO.setEmployeeDTO(employeeDTO);
