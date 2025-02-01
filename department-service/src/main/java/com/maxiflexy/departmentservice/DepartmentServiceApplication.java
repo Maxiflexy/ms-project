ce529ad025be9628e6db309af2abc8b338151ac5
Maxiflexy <onyemax247@gmail.com>
2025-02-01 18:28:17 +0100

    added euraka client server

--- department-service/src/main/java/com/maxiflexy/departmentservice/DepartmentServiceApplication.java
+++ department-service/src/main/java/com/maxiflexy/departmentservice/DepartmentServiceApplication.java
@@ -3,6 +3,7 @@ package com.maxiflexy.departmentservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class DepartmentServiceApplication {
 
